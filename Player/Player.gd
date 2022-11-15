2339e767357b98be444043e9660dc7bbaa0bf15e
Chris <cansler7@gmail.com>
2022-11-14 20:43:16 -0500

    Progress up through video #6

---
+++ Player/Player.gd
@@ -0,0 +1,38 @@
+extends Area2D
+
+onready var animatedSprite:AnimatedSprite = $AnimatedSprite
+# onready var sprite:Node2D = get_node("Sprite")
+
+export var speed: float = 100
+var vel: Vector2 = Vector2(0,0)
+
+func _process(delta):
+	# Animate
+	if vel.x < 0:
+		animatedSprite.play("Left")
+	elif vel.x > 0:
+		animatedSprite.play("Right")
+	else:
+		animatedSprite.play("Straight")
+
+func _physics_process(delta):
+	var dirVec: Vector2 = Vector2(0,0)
+
+	if Input.is_action_pressed("move_left"): 
+		dirVec.x = -1;
+	elif Input.is_action_pressed("move_right"):
+		dirVec.x = 1;
+		
+	if Input.is_action_pressed("move_up"):
+		dirVec.y = -1;
+	elif Input.is_action_pressed("move_down"):
+		dirVec.y = 1;
+		
+	vel = dirVec.normalized() * speed
+	position += (vel * delta)
+	
+	# Make sure we are within viewport
+	var viewRect:Rect2 = get_viewport_rect()
+	position.x = clamp(position.x, 0, viewRect.size.x)
+	position.y = clamp(position.y, 0, viewRect.size.y)
+		
